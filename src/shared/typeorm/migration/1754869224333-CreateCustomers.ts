9ac08e4d6d935ba0cbb24520d0a1369676d0896a
Ricksrss <henrique.hsoares@gmail.com>
2025-08-10 20:50:42 -0300

    feat: customers migration

---
+++ src/shared/typeorm/migration/1754869224333-CreateCustomers.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCustomers1754869224333 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'customers',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true, // garante que não haja dois clientes com o mesmo email
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('customers');
+  }
+}
